f288e4d591f1055edb832be4fb2a19c18cb6761f
Jeffrey Simon <jdsimon@gmail.com>
2016-05-21 21:07:03 -0400

    initial commit

---
+++ test/photomosaic_generator/core_test.clj
@@ -0,0 +1,7 @@
+(ns photomosaic-generator.core-test
+  (:require [clojure.test :refer :all]
+            [photomosaic-generator.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
